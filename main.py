ca540bdc80ec1128ce6676df35a1ecc966a6418c
Srujana Rachakonda <srachak@ncsu.edu>
2020-04-03 17:06:36 -0400

    Modified data after PCA

--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import preprocessing
 
 
 def main():
-
-    X_train, y_train, X_test, y_test = preprocessing.load_data('./parkinsons_updrs.csv')
-    X_train, X_test = preprocessing.extract_features(X_train, y_train)
+    X_train, X_test, y_train, y_test = preprocessing.load_data('./parkinsons_updrs.csv')
+    X_train, X_test = preprocessing.extract_features(X_train, X_test)
 
 
 if __name__ == "__main__":
